1f8ff96f8bb889a40fd313c0f363b6f0ec6dfcaf
Sameer Ahmad Siddiqui <sam@cloudera.com>
2018-06-17 23:15:41 -0400

    Uodate write parquet

---
+++ src/main/scala/spark/main/Main.scala
@@ -0,0 +1,16 @@
+package spark.main
+import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.internal.Logging
+import spark.likhai.WriteParquet
+import spark.padhai.ReadJson
+
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession.builder.master("local").getOrCreate()
+    println(spark.sessionState)
+    val data = ReadJson.readJsonFile(spark,"input/json/*.json")
+    data.printSchema()
+    WriteParquet.writeParquetFile(spark,data,"output/parquet/test",SaveMode.Append)
+
+}}
