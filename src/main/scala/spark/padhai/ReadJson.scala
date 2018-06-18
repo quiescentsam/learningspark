15b4142dd43430ff2f245c990568cd77f14f535b
Sameer Ahmad Siddiqui <sam@cloudera.com>
2018-06-17 21:13:55 -0400

    first commit

--- src/main/scala/spark/padhai/ReadJson.scala
+++ src/main/scala/spark/padhai/ReadJson.scala
@@ -6,8 +6,13 @@ object ReadJson {
 
   def main(args: Array[String]): Unit = {
     val spark = SparkSession.builder.master("local").getOrCreate()
-    val jobj = spark.read.json("input/json/*")
-    val name = jobj.select(explode(jobj("restaurant")))
+//    val jobj = spark.read.json("input/json/*.json")
+//    jobj.printSchema()
+//    jobj.write.mode("append").parquet("output/parquet/sample.parq")
+    val tab = spark.read.parquet("output/parquet/sample.parq")
+    tab.printSchema()
+    println(tab.count)
+    tab.show()
   }
 
 
