db87e8819b9deacad16f0a643f5a52b9661c6bb5
Sameer Ahmad Siddiqui <sam@cloudera.com>
2018-06-18 20:52:12 -0400

    app properties

--- src/main/scala/spark/main/Main.scala
+++ src/main/scala/spark/main/Main.scala
@@ -1,9 +1,10 @@
 package spark.main
+import org.apache.hadoop.conf.Configuration
 import org.apache.spark.sql.{SaveMode, SparkSession}
 import org.apache.spark.internal.Logging
 import spark.likhai.WriteParquet
 import spark.padhai.ReadJson
-
+import org.apache.hadoop.fs.{FileSystem, Path}
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -15,10 +16,15 @@ object Main {
       .getOrCreate()
 
 
+//
+//    println(spark.sessionState)
+//    val data = ReadJson.readJsonFile(spark,"input/json/*.json")
+//    data.printSchema()
+//    WriteParquet.writeParquetFile(spark,data,"output/parquet/test",SaveMode.Append)
 
-    println(spark.sessionState)
-    val data = ReadJson.readJsonFile(spark,"input/json/*.json")
-    data.printSchema()
-    WriteParquet.writeParquetFile(spark,data,"output/parquet/test",SaveMode.Append)
 
+    val conf = new Configuration()
+    val fileSystem = FileSystem.get(conf)
+    fileSystem.rename("part-00000-2d68a51e-efc3-449d-8f98-e5e85bc9e513-c000.snappy.parquet","part-00000-2d68a51e-efc3-449d-8f98-e5e85bc9e513-c000.snappy.parquet2")
+    //fileSystem.rename(new Path(existinghdfs_dirpath+oldname), new Path(newhdfs_dirPath+newname))
 }}
