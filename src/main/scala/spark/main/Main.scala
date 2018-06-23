4721d593744139a1326b4456daa0ec523f75e2c6
Sameer Ahmad Siddiqui <sam@cloudera.com>
2018-06-23 19:45:02 -0400

    2018062319

--- src/main/scala/spark/main/Main.scala
+++ src/main/scala/spark/main/Main.scala
@@ -13,18 +13,26 @@ object Main {
       .master("local")
       .appName("Test App")  // Application Name
       .enableHiveSupport()  // to enable HiveContext
+     // .config(spark.sql.parquet.mergeSchema,true)
       .getOrCreate()
 
 
+
+    println(spark.sessionState)
+    val data = ReadJson.readJsonFile(spark,"input/json/sar2.json")
+    data.printSchema()
+    WriteParquet.writeParquetFile(spark,data,"output/parquet/test",SaveMode.Append)
+    spark.read.option("mergeSchema", "true").parquet("output/parquet/test").printSchema()
+    println(spark.read.parquet("output/parquet/test").count())
+
 //
-//    println(spark.sessionState)
-//    val data = ReadJson.readJsonFile(spark,"input/json/*.json")
-//    data.printSchema()
-//    WriteParquet.writeParquetFile(spark,data,"output/parquet/test",SaveMode.Append)
-
-
-    val conf = new Configuration()
-    val fileSystem = FileSystem.get(conf)
-    fileSystem.rename('/user','/user/')
-    //fileSystem.rename(new Path(existinghdfs_dirpath+oldname), new Path(newhdfs_dirPath+newname))
+//    val conf = new Configuration()
+//    val fileSystem = FileSystem.get(conf)
+//    fileSystem.copyFromLocalFile('/input/json','/user/sameer')
+//    fileSystem.rename(org.apache.hadoop.fs.Path,org.apache.hadoop.fs.Path)
+//    //fileSystem.rename(new Path(existinghdfs_dirpath+oldname), new Path(newhdfs_dirPath+newname))
 }}
+
+
+
+
